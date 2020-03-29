8d630addc508a1e2f573bae52ff3d39d238a8d26
Jackie Blas <jackieblas5@yahoo.com>
2020-03-29 07:20:45 +0000

    fish files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,37 @@
 # setting environment variables
-set -gx EDITOR code
+set -gx ARCHFLAGS '-arch x86_64'
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx PAGER less
+set -gx TERMINAL terminator
+set -gx VISUAL code
+set -gx EDITOR $VISUAL
+set -gx GIT_EDITOR $VISUAL
+set -gx XDG_CURRENT_DESKTOP GNOME
 
-# defining abbr
+#turns on 24 bit support
+set -gx fish_term24bit 1
+
+set -gx fish_greeting "This is a trap"
+
+# defining third party abbreviations
 abbr -a manjaro_usb sudo dd bs=4M if=manjaro.iso of=/dev/sdX status=progress oflag=sync
 abbr -a dotfiles /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME
+abbr -a fish_config $EDITOR $HOME/.config/fish/config.fish
+abbr -a refresh source $HOME/.config/fish/config.fish
+abbr -a prompt $EDITOR $HOME/.config/fish/functions/fish_prompt.fish
+abbr -a public_ip curl -s icanhazip.com
+abbr -a cclip xclip -selection clipboard
+
+# defining unix abbreviations
+abbr -a ls ls -lh
+abbr -a lsa ls -lah
+abbr -a mkdir mkdir -p
+abbr -a mv mv -v
+abbr -a rm rm -rf
+abbr -a cp cp -r
+abbr -a scp scp -r
+
 
 # automatically start X server at login
 if status --is-login
@@ -11,4 +39,3 @@ if status --is-login
 		exec startx -- -keeptty
 	end
 end
-
---
+++ .config/fish/functions/fish_prompt.1585465329.copy
@@ -0,0 +1,14 @@
+function fish_prompt
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo -n (set_color blue)(prompt_pwd)' '
+
+    set_color -o
+    if test "$USER" = 'root'
+        echo -n (set_color red)'# '
+    end
+    echo -n (set_color '#f37a9b')'❯'(set_color '#fafdd3')'❯'(set_color '#98f5ff')'❯ '
+    set_color normal
+end
